4990649d3521ce15b78022dea2dbbf65212f497e
alfieteam <alfieteam1993@gmail.com>
2017-12-27 23:04:50 +0200

    first commit

---
+++ 2/2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE>
+<html>
+<head>
+</head>
+<body>
+<?php
+
+$integer = 2;
+$double = -3.5;
+$string = "Привет";
+$boolean = true;
+
+echo "Переменая integer = $integer";
+echo "<br>";
+echo "Переменная double = $double";
+echo "<br>";
+echo "Переменная string = $string";
+echo "<br>";
+echo "Переменная boolean = $boolean";
+echo "<br>";
+echo 'В одинарных кавычках переменные не интерпритируются, например $integer .';
+
+?>
+</body>
+</html>
